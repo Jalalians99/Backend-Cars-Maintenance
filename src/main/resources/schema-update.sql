75bc483d2f2053045ac20b35af8fdcf10135717e
Ahmad Jalal <syedmuhammadjalal99@gmail.com>
2025-04-22 21:19:07 +0300

    Update schema-update.sql to change admin password

--- src/main/resources/schema-update.sql
+++ src/main/resources/schema-update.sql
@@ -5,3 +5,7 @@ ALTER TABLE IF EXISTS cars RENAME COLUMN last_oil_change_mileage TO mileage_at_f
 
 -- Update user_id column to owner_id for better clarity
 ALTER TABLE IF EXISTS cars RENAME COLUMN user_id TO owner_id;
+
+-- Update admin password to "thisisadminpassword"
+-- Using a BCrypt encoded password
+UPDATE users SET password = '$2a$10$g32qG5JBMFIZ9ww/93M/GeRj9/kfT4E.gZW4P6ZE4Z3ZZQpMns5Hi' WHERE username = 'admin';
